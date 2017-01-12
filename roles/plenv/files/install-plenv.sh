3da3155c597db18ddabbcceca5a3e121e172757f
Tsuyoshi HARA <chloe463+git@gmail.com>
2017-01-12 22:37:33 +0900

    fix(plenv): Fix git repository url (git:// -> https://)

--- roles/plenv/files/install-plenv.sh
+++ roles/plenv/files/install-plenv.sh
@@ -3,8 +3,8 @@
 export PLENV_ROOT=$HOME/.plenv
 
 if [[ ! -e $PLENV_ROOT ]];then
-    git clone git://github.com/tokuhirom/plenv.git $PLENV_ROOT
-    git clone git://github.com/tokuhirom/Perl-Build.git $PLENV_ROOT/plugins/perl-build/
+    git clone https://github.com/tokuhirom/plenv.git $PLENV_ROOT
+    git clone https://github.com/tokuhirom/Perl-Build.git $PLENV_ROOT/plugins/perl-build/
 fi
 $PLENV_ROOT/bin/plenv install 5.24.0
 $PLENV_ROOT/bin/plenv rehash
